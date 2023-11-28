436c79c02bd6e6a6164ee3b4f5d0ea0feebc2bd6
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-11-28 15:43:25 +0300

    Modify URLRequest file for work with JSON

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// MARK: - URLComponents
+
+var optUrlComponents = URLComponents()
+optUrlComponents.scheme = "https"
+optUrlComponents.host = "api.magicthegathering.io"
+optUrlComponents.path = "/v1/cards"
+optUrlComponents.queryItems = [
+    URLQueryItem(name: "name", value: "Opt"),
+]
+
+var blackLotusUrlComponents = URLComponents()
+blackLotusUrlComponents.scheme = "https"
+blackLotusUrlComponents.host = "api.magicthegathering.io"
+blackLotusUrlComponents.path = "/v1/cards"
+blackLotusUrlComponents.queryItems = [
+    URLQueryItem(name: "name", value: "Black Lotus"),
+]
+
+// MARK: - Call
+
+if let optURL = optUrlComponents.url, let blackLotusURL = blackLotusUrlComponents.url {
+    getData(urlRequest: optURL.absoluteString)
+    getData(urlRequest: blackLotusURL.absoluteString)
+} else {
+    print("Невозможно создать адрес")
+}
--- JSON.playground/Sources/URLRequest.swift
+++ JSON.playground/Sources/URLRequest.swift
@@ -6,21 +6,53 @@ public func getData(urlRequest: String) {
         print("Ссылка сформирована некорректно")
         return
     }
+    
     URLSession.shared.dataTask(with: url) { data, response, error in
-        if error != nil{
+        if error != nil {
             print("Ошибка: \(error!)")
+        }
+        
+        if let response = response as? HTTPURLResponse {
+            print("Код ответа от сервера: \(response.statusCode)")
         } else {
-            if let response = response as? HTTPURLResponse {
-                print("Код ответа от сервера: \(response.statusCode)")
+            print("Невозможно получить ответ от сервера")
+        }
+        
+        guard let data = data else { return }
+        
+        do {
+            let jsonDecoder = JSONDecoder()
+            let magicCard = try jsonDecoder.decode(Card.self, from: data)
+
+            if let optCard = magicCard.cards.first(where: { $0.name.lowercased() == "opt" }) {
+                print("""
+                Имя карты: \(optCard.name)
+                Мановая стоимость: \(optCard.manaCost ?? "N/A")
+                Конвертированная мановая стоимость: \(optCard.cmc)
+                Тип карты: \(optCard.type)
+                Редкость: \(optCard.rarity)
+                Название набора: \(optCard.setName)
+                Набор карт: \(optCard.cardSet)
+                Текст: \(optCard.text)
+                Художник: \(optCard.artist)
+                """)
+            } else if let blackLotusCard = magicCard.cards.first(where: { $0.name.lowercased() == "black lotus" }) {
+                print("""
+                Имя карты: \(blackLotusCard.name)
+                Мановая стоимость: \(blackLotusCard.manaCost ?? "N/A")
+                Конвертированная мановая стоимость: \(blackLotusCard.cmc)
+                Тип карты: \(blackLotusCard.type)
+                Редкость: \(blackLotusCard.rarity)
+                Название набора: \(blackLotusCard.setName)
+                Набор карт: \(blackLotusCard.cardSet)
+                Текст: \(blackLotusCard.text)
+                Художник: \(blackLotusCard.artist)
+                """)
             } else {
-                print("Невозможно получить ответ от сервера")
+                print("Карта не найдена")
             }
-            
-            if let data = data, let dataAsString = String(data: data, encoding: .utf8) {
-                print("Данные, пришедшие от сервера: \(dataAsString)")
-            } else {
-                print("Данные не получены или не могут быть преобразованы в нужный формат")
-            }
+        } catch {
+            print("Ошибка при декодировании данных: \(error)")
         }
     }.resume()
 }
